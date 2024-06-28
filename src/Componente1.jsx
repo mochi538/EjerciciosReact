a117ce177a73d94c4f11bb23bcff49340c3e3451
Onixdrew <andrewmosquera1811@gmail.com>
2024-06-28 16:14:49 -0500

    segundo commit

---
+++ src/Componente1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Componente1(){
+    return (
+        <>
+            <h1>Hola hola</h1>
+            
+        </>
+
+    )
+};
---
+++ src/Componente2.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Componente2(){
+    return (
+        <>
+            <button>Click me ;3</button>
+        </>
+
+    )
+};
